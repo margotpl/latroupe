4b99968366974b421da5556b716345e4859aa500
margotpl <planquemargot@gmail.com>
2015-10-22 17:55:53 +0200

    VF2
    VF2

---
+++ global.js
@@ -0,0 +1,28 @@
+// Custom JS
+$(document).ready(function(){
+  // Add your code here
+});
+// Custom JS
+$(document).ready(function(){
+  /* ==========================================================================
+     Simple Form - Contact Form
+     ========================================================================== */
+
+  $('#mailbox').submit(function(){
+    $.ajax({
+      dataType: 'jsonp',
+      url: "http://getsimpleform.com/messages/ajax?form_api_token=038e22d05589c37eaafbcf396cfd581b",
+      data: $("#mailbox").serialize(),
+      error: function() {
+        $('#mailbox').hide();
+        $('#formErrorMessage').show();
+      }
+    }).done(function() {
+      //callback which can be used to show a thank you message
+      //and reset the form
+      $('#mailbox').hide();
+      $('#formSuccessMessage').show();
+    });
+    return false; //to stop the form from submitting
+  });
+});
